3c772d6c407e15cd398ff9111decbb055a38c9f0
LordCartel007 <cartellord77@gmail.com>
2025-01-22 16:02:08 +0100

    ecommerce front

---
+++ ecommerce-front/pages/product/[id].js
@@ -0,0 +1,14 @@
+import Center from "../../components/Center";
+import Header from "../../components/Header";
+import Title from "../../components/Title";
+
+export default function ProductPage({}) {
+  return (
+    <>
+      <Header />
+      <Center>
+        <Title>Product</Title>
+      </Center>
+    </>
+  );
+}
